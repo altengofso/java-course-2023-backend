d5d1a4a87e588eb7339185f3d6a1e2eede01c022
altengofso <altengofso@gmail.com>
2024-03-10 20:59:01 +0300

    solve hw4 tasks

---
+++ scrapper/src/main/resources/migrations/sql/01_init_tables.sql
@@ -0,0 +1,21 @@
+create table if not exists link
+(
+    id              bigserial primary key,
+    url             text unique not null,
+    last_check_at   timestamp with time zone not null default now(),
+    updated_at      timestamp with time zone not null
+);
+
+create table if not exists chat
+(
+    id              bigint primary key,
+    created_at      timestamp with time zone not null default now()
+);
+
+create table if not exists chat_link
+(
+    link_id         bigint references link ON DELETE CASCADE,
+    chat_id         bigint references chat ON DELETE CASCADE,
+    created_at      timestamp with time zone not null default now(),
+    primary key (link_id, chat_id)
+);
