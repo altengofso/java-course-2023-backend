cafe6d41946cdaed8cdc52cfa7128bd2634b7918
altengofso <altengofso@gmail.com>
2024-03-18 04:24:29 +0300

    fix test class name

--- scrapper/src/test/java/edu/java/scrapper/app/repository/JdbcChatRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/app/repository/JdbcChatRepositoryTest.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class JdbcLinkChatRepositoryTest extends IntegrationEnvironment {
+public class JdbcChatRepositoryTest extends IntegrationEnvironment {
     @Autowired
     private JdbcChatRepository jdbcChatRepository;
 
