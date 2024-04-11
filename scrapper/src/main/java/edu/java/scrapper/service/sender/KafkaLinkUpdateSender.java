08b23825e1869745e929ca2c207a0daf90608b28
altengofso <altengofso@gmail.com>
2024-04-12 01:08:10 +0300

    task 5, 6

--- scrapper/src/main/java/edu/java/scrapper/service/sender/KafkaLinkUpdateSender.java
+++ scrapper/src/main/java/edu/java/scrapper/service/sender/KafkaLinkUpdateSender.java
@@ -3,8 +3,10 @@ package edu.java.scrapper.service.sender;
 import edu.java.scrapper.client.botclient.models.LinkUpdate;
 import edu.java.scrapper.configuration.ApplicationConfig;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 
+@Log4j2
 @RequiredArgsConstructor
 public class KafkaLinkUpdateSender implements LinkUpdateSenderService {
     private final KafkaTemplate<String, LinkUpdate> kafkaTemplate;
@@ -12,6 +14,7 @@ public class KafkaLinkUpdateSender implements LinkUpdateSenderService {
 
     @Override
     public void send(LinkUpdate update) {
+        log.info("Sending link update to kafka topic: {}", update);
         kafkaTemplate.send(applicationConfig.updatesTopic().name(), update);
     }
 }
